4928afb0c19609a12566d7f2d79d2aff38cd421e
MrDave1999 <daveseva2010@hotmail.es>
2021-11-06 13:05:36 -0500

    Added Exceptions module

---
+++ src/SeztionParser/Exceptions/ParserException.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeztionParser.Exceptions
+{
+    /// <summary>
+    /// The exception that is thrown when the parser finds an error during the parsing, such as a duplicate section.
+    /// </summary>
+    public class ParserException : Exception
+    {
+        private readonly object actualValue;
+
+        /// <summary>
+        /// Allows access to the actual value causing the exception.
+        /// </summary>
+        public object ActualValue => actualValue;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParserException" /> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public ParserException(string message) : base(message)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParserException" /> class with a specified error message and the actual value that caused the exception.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="actualValue">The actual value that caused the exception.</param>
+        public ParserException(string message, object actualValue) : base(message)
+        {
+            this.actualValue = actualValue;
+        }
+
+        /// <inheritdoc  />  
+        public override string Message
+            => actualValue != null ? base.Message + " (Actual Value: " + actualValue + ")" : base.Message;
+    }
+}
---
+++ src/SeztionParser/Exceptions/SectionNotFoundException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeztionParser.Exceptions
+{
+    /// <summary>
+    /// The exception that is thrown when the specified section does not exist in the collection.
+    /// </summary>
+    public class SectionNotFoundException : ArgumentException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SectionNotFoundException" /> class with a specified error message and the name of the parameter that causes this exception.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="paramName">The name of the parameter that caused the exception.</param>
+        public SectionNotFoundException(string message, string paramName) : base(message, paramName)
+        {
+
+        }
+    }
+}
