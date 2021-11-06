1a2fc324fb9ec31312545b011e7dd5965fb7efad
MrDave1999 <daveseva2010@hotmail.es>
2021-11-06 13:06:09 -0500

    Added Interfaces module

---
+++ src/SeztionParser/Interfaces/ISectionsParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SeztionParser.Exceptions;
+
+namespace SeztionParser.Interfaces
+{
+    /// <summary>
+    /// Represents the sections parser.
+    /// </summary>
+    public interface ISectionsParser
+    {
+        /// <summary>
+        /// Starts the parsing to extract the data of each section.
+        /// </summary>
+        /// <param name="data">Data to parsing.</param>
+        /// <exception cref="ParserException">If the parser find an error during the parsing.</exception>
+        /// <returns>An instance with the parsed data.</returns>
+        ISectionsData Parse(string data);
+    }
+}
