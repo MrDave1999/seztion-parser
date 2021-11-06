9ca47a85a62488f3a3c35ac9a0c6536f2c8ac92a
MrDave1999 <daveseva2010@hotmail.es>
2021-11-06 13:06:50 -0500

    Added Providers module

---
+++ src/SeztionParser/Providers/SectionsData.cs
@@ -0,0 +1,100 @@
+﻿using SeztionParser.Exceptions;
+using SeztionParser.Interfaces;
+using SeztionParser.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using static SeztionParser.Constants.ExceptionMessages;
+
+namespace SeztionParser.Providers
+{
+    /// <inheritdoc cref="ISectionsData" />
+    /// <remarks>
+    /// This class uses the <see cref="IDictionary{TKey,TValue}" /> interface, where <c>TKey</c> represents the name of the section and the <c>TValue</c> represents the data of the section.
+    /// </remarks>
+    class SectionsData : ISectionsData
+    {
+        /// <summary>
+        /// Allows access to the data of each section.
+        /// </summary>
+        /// <value>
+        /// The dictionary with the data of each section.
+        /// </value>
+        private readonly Dictionary<string, ISectionData> sections = new Dictionary<string, ISectionData>();
+
+        /// <inheritdoc />
+        public ISectionData this[string section]
+        {
+            get
+            {
+                if (sections.TryGetValue(section, out var value))
+                    return value;
+                throw new SectionNotFoundException(SpecifiedSectionDoesNotExistMessage, nameof(section));
+            }
+        }
+
+        /// <summary>
+        /// Adds data to a section.
+        /// </summary>
+        /// <param name="section">The name of the section to add.</param>
+        /// <param name="data">The data to be added to the section.</param>
+        /// <returns><c>true</c> if the data can be added, otherwise <c>false</c>.</returns>
+        /// <remarks>
+        /// If the data cannot be added, it is because the <c>section</c> parameter is <c>null</c> or the section already exists in the collection.
+        /// </remarks>
+        public bool Add(string section, ISectionData data)
+        {
+            try
+            {
+                sections.Add(section, data);
+                return true;
+            }
+            catch(ArgumentException)
+            {
+                return false;
+            }
+        }
+
+        /// <inheritdoc />
+        public bool TryGetData(string section, out ISectionData data)
+            => sections.TryGetValue(section, out data);
+
+        /// <inheritdoc />
+        public ICollection<string> GetNames()
+            => sections.Keys;
+
+        /// <inheritdoc />
+        public ICollection<ISectionData> GetAll()
+            => sections.Values;
+
+        /// <summary>
+        /// Return an enumerator used to traverse all sections of the collection.
+        /// </summary>
+        /// <returns>An enumerator used to traverse all sections of the collection.</returns>
+        public IEnumerator<SectionModel> GetEnumerator()
+        {
+            foreach(var section in sections)
+                yield return new SectionModel(section.Key, section.Value);
+        }
+
+        /// <summary>
+        /// Return an enumerator used to traverse all sections of the collection.
+        /// </summary>
+        /// <returns>An enumerator used to traverse all sections of the collection.</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        /// <summary>
+        /// Converts the <see cref="SectionsData" /> instance to a <see cref="String" /> object.
+        /// </summary>
+        /// <returns>A string that represents the current object.</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach(var section in this)
+                sb.Append(section.ToString());
+            return sb.ToString();
+        }
+    }
+}
---
+++ src/SeztionParser/Providers/SectionsParser.cs
@@ -0,0 +1,87 @@
+﻿using SeztionParser.Interfaces;
+using static System.Environment;
+using SeztionParser.Constants;
+using SeztionParser.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeztionParser.Providers
+{
+    /// <summary>
+    /// Defines the operations that control the parser behavior.
+    /// </summary>
+    public class SectionsParser : ISectionsParser
+    {
+        /// <summary>
+        /// Check if the text is a comment.
+        /// </summary>
+        /// <param name="text">The text to validate.</param>
+        /// <returns><c>true</c> if is comment, otherwise <c>false</c>.</returns>
+        protected virtual bool IsComment(string text)
+            => text.TrimStart()[0] == '#';
+
+        /// <summary>
+        /// Check if the section has the correct format.
+        /// </summary>
+        /// <param name="text">The text to validate.</param>
+        /// <returns><c>true</c> if the section is correctly format, otherwise <c>false</c>.</returns>
+        protected virtual bool IsSection(ref string text)
+        {
+            text = text.Trim();
+            return text[0] == '[' && text[text.Length - 1] == ']';
+        }
+
+        /// <summary>
+        /// Extracts the name of the section.
+        /// </summary>
+        /// <param name="text">The section to extract.</param>
+        /// <returns>The name of the extracted section.</returns>
+        protected virtual string ExtractSection(string text)
+             => text.Substring(1, text.Length - 2);
+
+        /// <inheritdoc />
+        public ISectionsData Parse(string data)
+        {
+            if (string.IsNullOrWhiteSpace(data))
+                throw Create(ExceptionMessages.DataSourceIsEmptyMessage);
+            var lines = data.Split(NewLine.ToCharArray());
+            var sections = new SectionsData();
+            SectionData sectionData = null;
+            string sectionName = null;
+            int len = lines.Length;
+            for (int i = 0; i < len; ++i)
+            {
+                if (string.IsNullOrWhiteSpace(lines[i]) || IsComment(lines[i]))
+                    continue;
+                if (IsSection(ref lines[i]))
+                {
+                    bool IsEmptyPreviousSection = sectionData?.Count == 0;
+                    if (IsEmptyPreviousSection)
+                        throw Create(ExceptionMessages.SectionWithoutDataMessage, sectionName);
+
+                    sectionName = ExtractSection(lines[i]);
+                    if (string.IsNullOrWhiteSpace(sectionName))
+                        throw Create(ExceptionMessages.SectionNameIsEmptyMessage);
+                    sectionData = new SectionData();
+                    bool isRepeatedSection = !sections.Add(sectionName, sectionData);
+                    if (isRepeatedSection)
+                        throw Create(ExceptionMessages.SeccionIsRepeatedMessage, sectionName);
+                }
+                else
+                {
+                    if (sectionData == null)
+                        throw Create(ExceptionMessages.ElementThatIsNotPartAnySectionMessage, lines[i]);
+                    sectionData.Add(lines[i]);
+                }
+            }
+            bool IsEmptyLastSection = sectionData.Count == 0;
+            if (IsEmptyLastSection)
+                throw Create(ExceptionMessages.SectionWithoutDataMessage, sectionName);
+            return sections;
+        }
+
+        private Exception Create(string message) => new ParserException(message);
+        private Exception Create(string message, object actualValue) => new ParserException(message, actualValue);
+    }
+}
