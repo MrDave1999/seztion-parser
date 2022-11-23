5a73aa1d3ed147973b22c5130c32e891e8ffc6bf
MrDave1999 <daveseva2010@hotmail.es>
2022-11-23 13:19:07 -0500

    Fixed warning missing XML comment for publicly visible type or member 'Type_or_Member'

--- src/SeztionParser/Constants/ExceptionMessages.cs
+++ src/SeztionParser/Constants/ExceptionMessages.cs
@@ -9,16 +9,10 @@ namespace SeztionParser.Constants;
 /// </summary>
 public class ExceptionMessages
 {
-    ///
     public const string SectionNameIsEmptyMessage = "Parser found an empty section name";
-    ///
     public const string SectionWithoutDataMessage = "Parser found a section without data"; 
-    ///
     public const string SpecifiedSectionDoesNotExistMessage = "The specified section does not exist";
-    ///
     public const string SeccionIsRepeatedMessage = "Parser found a repeated section";
-    ///
     public const string ElementThatIsNotPartAnySectionMessage = "Parser found an element that is not part of any section";
-    ///
     public const string DataSourceIsEmptyMessage = "Parser found an empty data source";
 }
