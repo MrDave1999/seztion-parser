bbabf001d071750115cb60baf1a99268008f9b9a
MrDave1999 <daveseva2010@hotmail.es>
2024-07-24 19:11:33 -0500

    Create ThrowHelper type for guard clauses

---
+++ src/SeztionParser/Common/ThrowHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace SeztionParser;
+
+internal class ThrowHelper
+{
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException"/> if <c>argument</c> is <c>null</c>.
+    /// </summary>
+    /// <param name="argument">
+    /// The reference type argument to validate as non-null.
+    /// </param>
+    /// <param name="paramName">
+    /// The name of the parameter with which argument corresponds.
+    /// </param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static void ThrowIfNull(object argument, string paramName)
+    {
+        if (argument is null)
+            throw new ArgumentNullException(paramName);
+    }
+}
