284227c011d47691a6e939217d2118d09e74d8aa
MrDave1999 <daveseva2010@hotmail.es>
2024-07-26 15:57:05 -0500

    test: Create unit tests for ParserException type

---
+++ src/SeztionParser.Tests/Parser/ParserExceptionTests.cs
@@ -0,0 +1,35 @@
+﻿namespace SeztionParser.Tests.Parser;
+
+[TestClass]
+public class ParserExceptionTests
+{
+    [TestMethod]
+    public void Message_WhenActualValueIsDefined_ShouldReturnsFormattedMessage()
+    {
+        // Arrange
+        var message = ExceptionMessages.SeccionIsRepeatedMessage;
+        var actualValue = "Section";
+        var exception = new ParserException(message, actualValue);
+        var expectedMessage = $"{message} (Actual Value: Section)";
+
+        // Act
+        var actual = exception.Message;
+
+        // Assert
+        actual.Should().Be(expectedMessage);
+    }
+
+    [TestMethod]
+    public void Message_WhenActualValueIsNotDefined_ShouldReturnsDefaultMessage()
+    {
+        // Arrange
+        var exception = new ParserException();
+        var expectedMessage = ExceptionMessages.ParserDefaultMessage;
+
+        // Act
+        var actual = exception.Message;
+
+        // Assert
+        actual.Should().Be(expectedMessage);
+    }
+}
