38497adfaa92c48254ee9fec6b33d1aaf7e658eb
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2024-07-26 19:46:58 -0500

    Create missing unit tests (#21)

---
+++ src/SeztionParser.Tests/Reader/SectionModelTests.cs
@@ -0,0 +1,91 @@
+﻿namespace SeztionParser.Tests.Reader;
+
+[TestClass]
+public class SectionModelTests
+{
+    [TestMethod]
+    public void Constructor_WhenNameIsNull_ShouldThrowArgumentNullException()
+    {
+        // Arrange
+        var name = default(string);
+        var data = new SectionData();
+
+        // Act
+        Action act = () => {
+            var model = new SectionModel(name, data);
+        };
+
+        // Assert
+        act.Should()
+           .Throw<ArgumentNullException>()
+           .WithParameterName(nameof(name));
+    }
+
+    [TestMethod]
+    public void Constructor_WhenDataIsNull_ShouldThrowArgumentNullException()
+    {
+        // Arrange
+        var name = "Section";
+        var data = default(ISectionData);
+
+        // Act
+        Action act = () => {
+            var model = new SectionModel(name, data);
+        };
+
+        // Assert
+        act.Should()
+           .Throw<ArgumentNullException>()
+           .WithParameterName(nameof(data));
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(SectionContainsData), DynamicDataSourceType.Property)]
+    public void ToString_WhenSectionContainsData_ShouldReturnsString(ISectionData data, string expectedString)
+    {
+        // Arrange
+        var name = "Position";
+        var model = new SectionModel(name, data);
+
+        // Act
+        var actual = model.ToString();
+
+        // Assert
+        actual.Should().Be(expectedString);
+    }
+
+    static IEnumerable<object[]> SectionContainsData
+    {
+        get
+        {
+            return
+            [
+                [
+                    new SectionData { "1008", "1009" },
+                    """
+                    Section: Position ->
+                    [
+                       1008
+                       1009
+                    ]
+
+                    """
+                ],
+                [
+                    new SectionData { "1008" },
+                    """
+                    Section: Position -> [1008]
+
+                    """
+                ],
+                [
+                    new SectionData(),
+                    """
+                    Section: Position -> []
+
+                    """
+                ]
+            ];
+        }
+    }
+}
